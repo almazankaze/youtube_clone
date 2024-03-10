0c52e09af3c13ca66a0ab1221d3effefba1d5d86
John Almazan <almazankaze@live.com>
2024-03-10 16:32:33 -0500

    Update Sidebar.jsx

--- youtube-project/src/components/sidebar/Sidebar.jsx
+++ youtube-project/src/components/sidebar/Sidebar.jsx
@@ -9,7 +9,39 @@ import Sub from "../../assets/sub.jpg";
 import "./sidebar.scss";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return <div className="sidebar>
+  <div className="shortcut-links">
+  <div className="side-link">
+  <p>Home</p>
+  </div>
+    <div className="side-link">
+  <p>Home</p>
+  </div>
+    <div className="side-link">
+  <p>Gaming</p>
+  </div>
+    <div className="side-link">
+  <p>Automobiles</p>
+  </div>
+    <div className="side-link">
+  <p>Sports</p>
+  </div>
+    <div className="side-link">
+  <p>Music</p>
+  </div>
+    <div className="side-link">
+  <p>News</p>
+  </div>
+    <hr />
+    
+  </div>
+    <div className="subscribed-list">
+    <h3>Subscribed</h3>
+    <div className="side-link">
+  <p>0Kazekun0</p>
+  </div>
+  </div>
+  </div>;
 };
 
 export default Sidebar;
